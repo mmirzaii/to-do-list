0b510c8718ee983f40ed2feea3c0ce2ad06dc70c
mmd <m.mirzaee0101@gmail.com>
2024-05-25 22:58:45 +0330

    font added

---
+++ src/page/Authentication.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+class Authentication extends Component {
+  state = {};
+  render() {
+    return <h1>Authentication</h1>;
+  }
+}
+
+export default Authentication;
