f1615823f84e52a20a7bf947a50b444cd81dc9f4
mmd <m.mirzaee0101@gmail.com>
2024-05-25 22:27:56 +0330

    install tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import Authentication from "./page/Authentication";
+import "./style.css";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="Authentication" element={<Authentication />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
